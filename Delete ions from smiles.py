4cfd07ee241462ccf920e6532af99093b474b513
Linas <68922285+DataAIchemist@users.noreply.github.com>
2022-09-02 09:38:07 +0300

    Update and rename Delete ions from smiles to Delete ions from smiles.py

--- Delete ions from smiles.py
+++ Delete ions from smiles.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 
-f = open("/home/linas/Downloads/S1PR1_Chembl_all.csv", "r")
+f = open("S1PR1_Chembl_all.csv", "r")
 df = pd.read_csv(f, sep=";")
 
 dfSmiles = df["Smiles"]
-fn = open("/home/linas/Downloads/TrueSmiles_new.txt", "w")
+fn = open("TrueSmiles_new.txt", "w")
 
 for n, l in dfSmiles.iteritems():
     if "." in l:
